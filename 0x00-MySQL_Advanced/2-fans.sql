1ac07c3d7b8090ee474a1842c76a6ce8582d61ae
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-14 22:45:01 +0300

    Update 2-fans.sql
    - Corrected SQL query for ranking country origins by fans

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Script to rank country origins of bands by number of fans
 
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin AS origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
