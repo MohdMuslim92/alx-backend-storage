e962e6a0fbee3d3f9a3e269ac04d2a3d135b167d
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-15 00:37:55 +0300

    Update 100-average_weighted_score.sql
    - Corrected sql query

--- 0x00-MySQL_Advanced/100-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -1,28 +1,17 @@
 -- Create the ComputeAverageWeightedScoreForUser procedure
 
-DELIMITER //
-
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+delimiter //
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser (
+		IN user_id INT
+)
 BEGIN
-    DECLARE total_weighted_score FLOAT;
-    DECLARE total_weight FLOAT;
-    
-    -- Initialize variables
-    SET total_weighted_score = 0;
-    SET total_weight = 0;
-    
-    -- Calculate the weighted average score for the user
-    SELECT SUM(corrections.score * projects.weight) INTO total_weighted_score, SUM(projects.weight) INTO total_weight
-    FROM corrections
-    JOIN projects ON corrections.project_id = projects.id
-    WHERE corrections.user_id = user_id;
-    
-    -- Update the user's average score
-    IF total_weight > 0 THEN
-        UPDATE users SET average_score = total_weighted_score / total_weight WHERE id = user_id;
-    ELSE
-        UPDATE users SET average_score = 0 WHERE id = user_id;
-    END IF;
+	UPDATE users
+	SET average_score = (
+		SELECT SUM(corrections.score * projects.weight) / SUM(projects.weight)
+		FROM corrections
+		JOIN projects
+		ON projects.id = corrections.project_id AND corrections.user_id = user_id
+	)
+	WHERE id = user_id;
 END//
-
-DELIMITER ;
+delimiter ;
