19f63a607aa2912157489d9c339e72a3a36ed8f4
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-14 23:06:19 +0300

    Update 3-glam_rock.sql
    - Corrected SQL query for listing Glam rock bands by longevity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
 -- Script to list Glam rock bands ranked by longevity until 2022
 
-SELECT band_name,
-    CASE
-        WHEN formed = 0 OR split = 0 THEN 0
-        ELSE 2022 - GREATEST(formed, split)
-    END AS lifespan
+SELECT band_name, (IFNULL(split, YEAR(CURDATE()) - 1) - formed) AS lifespan
 FROM metal_bands
-WHERE style LIKE '%Glam rock%'
-ORDER BY lifespan DESC, band_name;
+WHERE FIND_IN_SET('Glam rock', style) > 0
+ORDER BY lifespan DESC;
