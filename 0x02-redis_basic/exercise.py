ac0ab967bc78ca0815e3a4ec8d76eef23675876a
Mohammed Saeed <mohammed.muslim2022@gmail.com>
2023-12-20 22:29:30 +0300

    Update exercise.py
    - Add the get, get_str, and get_int methods in the Cache class.
    - The get method retrieves data from Redis based on a key and applies an
      optional conversion function.
    - The get_str and get_int are specialized methods that automatically specify
      the conversion function for strings and integers respectively

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,7 +4,7 @@ Cache class utilizing Redis for storing data
 """
 import redis
 import uuid
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -28,3 +28,49 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(self, key: str,
+            fn: Callable = None) -> Union[str, bytes, int, None]:
+        """
+        Retrieves data from Redis using the provided key and applies optional
+        conversion function.
+
+        Args:
+        - key: The key to retrieve data from Redis.
+        - fn: Optional callable function to convert the retrieved data.
+
+        Returns:
+        - Retrieved data with optional conversion applied, or None if key
+        doesn't exist.
+        """
+        data = self._redis.get(key)
+        if data is None:
+            return None
+        if fn:
+            return fn(data)
+        return data
+
+    def get_str(self, key: str) -> Union[str, None]:
+        """
+        Retrieves and converts data from Redis to a string.
+
+        Args:
+        - key: The key to retrieve data from Redis.
+
+        Returns:
+        - Retrieved data as a string, or None if key doesn't exist.
+        """
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
+
+    def get_int(self, key: str) -> Union[int, None]:
+        """
+        Retrieves and converts data from Redis to an integer.
+
+        Args:
+        - key: The key to retrieve data from Redis.
+
+        Returns:
+        - Retrieved data as an integer, or None if key doesn't exist or data
+        cannot be converted.
+        """
+        return self.get(key, fn=lambda d: int(d))
